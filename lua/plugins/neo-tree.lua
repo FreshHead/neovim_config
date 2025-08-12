b20dc5d462ab3032f6156eb45875fa6e6286865a
Alexander Kostarev <UniverAlex@ya.ru>
2025-08-12 06:44:41 +0300

    use lsp inside neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -16,6 +16,7 @@ return {
   opts = {
     filesystem = {
       hide_dotfiles = false,
+      use_libuv_file_watcher = true,
       follow_current_file = {
         enabled = false, -- This will find and focus the file in the active buffer every time
       },
@@ -28,6 +29,38 @@ return {
         mappings = {
           ['\\'] = 'close_window',
         },
+      },
+    },
+    event_handlers = {
+      {
+        event = "file_renamed",
+        handler = function(args)
+          -- auto update imports
+          vim.lsp.buf.execute_command({
+            command = "_typescript.applyRenameFile",
+            arguments = {
+              {
+                sourceUri = vim.uri_from_fname(args.source),
+                targetUri = vim.uri_from_fname(args.destination),
+              },
+            },
+          })
+        end,
+      },
+      {
+        event = "file_moved",
+        handler = function(args)
+          -- auto update imports on move
+          vim.lsp.buf.execute_command({
+            command = "_typescript.applyRenameFile",
+            arguments = {
+              {
+                sourceUri = vim.uri_from_fname(args.source),
+                targetUri = vim.uri_from_fname(args.destination),
+              },
+            },
+          })
+        end,
       },
     },
   },
