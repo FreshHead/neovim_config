4acc9e7366d1f85f742c639701ca6daf82fae347
Alexander Kostarev <UniverAlex@ya.ru>
2025-10-04 08:01:03 +0300

    image copy pasting

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -26,4 +26,27 @@ return {
       }
     end,
   },
+  {
+    "HakonHarnes/img-clip.nvim",
+    event = "VeryLazy",
+    opts = {
+      default = {
+        embed_image_as_base64 = false,
+        prompt_for_file_name = true,
+        drag_and_drop = {
+          insert_mode = true,
+        },
+      },
+      filetypes = {
+        markdown = {
+          url_encode_path = true,
+          template = "![$CURSOR]($FILE_PATH)",
+          dir_path = "assets",
+        },
+      },
+    },
+    keys = {
+      { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste clipboard image" },
+    },
+  },
 }
