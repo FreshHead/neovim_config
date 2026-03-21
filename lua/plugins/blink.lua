7a3d84a0e738dc946828f39cf98730b6633cf9da
Alexander Kostarev <UniverAlex@ya.ru>
2026-03-21 15:16:57 +0300

    migrate into blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,7 +1,9 @@
 return {
   "saghen/blink.cmp",
   version = "*",
+  dependencies = { "L3MON4D3/LuaSnip" },
   opts = {
+    snippets = { preset = "luasnip" },
     sources = {
       default = { "lsp", "path", "snippets", "buffer" },
     },
